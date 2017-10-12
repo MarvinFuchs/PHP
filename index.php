b06d28ffd4bc79db3a5f33ca36fc52e3b549eca4
MarvinFuchs <32633397+MarvinFuchs@users.noreply.github.com>
2017-10-12 14:37:25 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,48 +1,47 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
+        <meta charset="utf-8"> 
         <link href="design.css" rel="stylesheet">       
         <title>
             Webseite1
         </title>
     </head>
-    <body>
-        <div class="outter-container">
-                
-            <div class="inner-container">
+    <body> 
+        <div class="outter-container">       
+            <div class="inner-container">      
                 <center>
                     <h1>
                         PHP-Beispielwebseite1
                     </h1> 
                 </center>
             </div>
-                
+            <h3> Link: </h3>
             <?php echo 'Dies soll ein Link zu <a href="index2.php">Webseite2</a> sein'; ?>
-        
             <ul>
                 <li>Thema 1</li>
                 <li>Theam 2</li>
                 <li>Thema 3</li>
                 <li>Thema 4</li>
             </ul>
-    
-            <p> Außerdem findet man hier noch ...</p>
-            </br>
-            <h3> ein Anmeldeformular </h3>
-            
-            <form action="index2.php">
+            <p> Außerdem findet man hier noch ein ...</p>
+            <br>
+            <h3> Formular </h3>
+            <form action="index.php">
                 <label>
                     Eingabe 
                     <input name="q">
-                </label>
-                
+                </label> 
                 <button>
                     eingeben
                 </button>
             </form>
-            </br>
-
+            <br>
+            
+            <p> Ausgabe: </p>
+            <br>
+            <?php echo $_GET[Herzlich Willkommen +'q'+, schön, dass Du Dich in das Seminar eingetragen hast!]; ?>
+            <br>
         </div>
     </body>
 </html>
--- index2.php
+++ index2.php
@@ -1,48 +1,47 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
+        <meta charset="utf-8"> 
         <link href="design.css" rel="stylesheet">       
         <title>
             Webseite2
         </title>
     </head>
-    <body>
-        <div class="outter-container">
-            
-            <div class="inner-container">
-                <center> 
+    <body> 
+        <div class="outter-container">       
+            <div class="inner-container">      
+                <center>
                     <h1>
                         PHP-Beispielwebseite2
-                    </h1>
+                    </h1> 
                 </center>
             </div>
-            
-            <?php echo 'Dies soll ein Link zu <a href="index.php">Webseite1</a> sein'; ?>
-            <?php echo $_GET['q']; ?>
+            <h3> Link: </h3>
+            <?php echo 'Dies soll ein Link zu <a href="index1.php">Webseite1</a> sein'; ?>
             <ul>
                 <li>Thema 1</li>
                 <li>Theam 2</li>
                 <li>Thema 3</li>
                 <li>Thema 4</li>
             </ul>
-
             <p> Außerdem findet man hier noch ein ...</p>
-            </br>
-            <h3> Anmeldeformular </h3>
-            
-            <form action="index.php">
+            <br>
+            <h3> Formular </h3>
+            <form action="index2.php">
                 <label>
                     Eingabe 
                     <input name="q">
-                </label>
-                
+                </label> 
                 <button>
                     eingeben
                 </button>
             </form>
-            </br>
-
+            <br>
+            
+            <p> Ausgabe: </p>
+            <br>
+            <?php echo $_GET[Herzlich Willkommen +'q'+, schön, dass Du Dich in das Seminar eingetragen hast!]; ?>
+            <br>
         </div>
     </body>
 </html>
